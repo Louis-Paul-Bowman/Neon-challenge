76c9c3a122026ed0ce1f713d6a9cc19ba182cbac
Louis-Paul Bowman <louispaul@ariseindustries.net>
2026-03-26 14:05:34 -0400

    add CV data

---
+++ microservices/agent/src/Docs/CV.py
@@ -0,0 +1,143 @@
+PLAINTEXT_CV = """
+Name: Louis-Paul Bowman
+
+LinkedIn URL: https://www.linkedin.com/in/louis-paul-bowman/
+GitHub URL: https://github.com/Louis-Paul-Bowman
+
+I am a co-founder of a Canadian startup offering end-to-end 
+development of Artificial Intelligence software products.
+I use my strong research background and software development skills
+to take cutting-edge AI models from academia to production.
+My work in helping to build a fledgling company has given me a unique combination of technical ability and business acumen. 
+
+I am seeking a senior AI developer or full-stack engineer role where I can build AI solutions that drive meaningful real-world impact.
+
+EDUCATION
+Master of Engineering 
+(M. Eng.)
+McGill University
+2021
+Bachelor of Engineering 
+(B. Eng.)
+McGill University 
+2019
+
+LANGUAGES
+English (native)
+French (native)
+
+KEY SKILLS
+Artificial Intelligence
+Training (TensorFlow, 5 years)
+NLP (5 years)
+SpaCy
+Supervised learning
+Generative modeling
+Transfer learning
+Embeddings
+RAGs
+Sourcing and labelling data
+Data science
+Literature review
+
+Backend development
+Python (6 years)
+Go (1 year)
+Software architecting
+APIs (Flask, FastAPI)
+Databases (SQL, vector, graph)
+ORMs
+Numerical analysis (numpy, scikit-learn, pandas, matplotlib)
+Quality Assurance
+Security and authentication
+Fuzzy logic
+
+Frontend development
+TypeScript / JavaScript (3 years)
+NodeJs / SvelteKit (3 years)
+Tailwind CSS
+Data visualization
+
+Deployment (DevOps)
+Docker
+Kubernetes
+Cloud computing (AWS, GCP)
+Unix (Linux)
+CI/CD (Github actions)
+Helm
+Ansible
+
+Professional skills
+Technical writing
+Communication
+Intellectual Property
+Working autonomously
+Developing as a team
+Managing employees
+
+WORK EXPERIENCE
+I am an NLP expert with deep experience training discriminative and generative architectures, working with embeddings, and implementing RAG systems. I have a strong background in machine learning, unsupervised training, and reinforcement learning. 
+
+For the last 4 years, our company has been working for the RCMP and SSC to develop Sherlock: a cloud-native AI-powered software which automates investigation of digital devices at scale.
+
+Arise Industries - Co-founder (Tech lead)
+2021 - Present
+Artificial Intelligence
+Led the full development cycle of AI components, from design to integration:
+•	Literature review and architecture selection.
+•	Training design including inputs/outputs, loss functions, metrics.
+•	Sourcing and sanitizing training data.
+•	Supervised and adversarial training. TensorFlow implementation including non-standard model layers, novel loss functions, and custom training loops.
+•	Model regularization.
+•	Hyperparameter tuning and model versioning.
+•	Quantitative model evaluation for performance and bias.
+•	Integration with backend systems.
+Developed AI components including:
+•	Multi-faceted information extraction pipelines: leveraging Named-Entity Recognition, Keyword Extraction, and TF-IDF.
+•	Password detection discriminator model.
+•	Many-to-many word-to-password style transfer: an Augmented CycleGAN model adapted from visual to textual domain.
+•	LLM-as-a-judge prototype: a Neo4j graph-RAG system that built a Knowledge Graph by cross-referencing PubMed publications against known high-risk universities, culminating in a fully-traceable LLM-generated risk assessment used to vet grants.
+Backend Development
+Architected and developed our company's distributed AI application: 
+•	Design and implementation of a microservice architecture.
+•	Development of python-based business logic and AI integrations.
+•	Code profiling and optimization of performance bottlenecks.
+•	Creation of Flask and FastAPI-based API routes.
+•	SQL database design, implementation, and optimization.
+•	Database integrations using ORMs.
+•	Creation of Quality Assurance end-to-end tests.
+
+Frontend development
+Designed and co-developed our application's web GUI:
+•	Implementation of a NodeJS-based client-server architecture GUI.
+•	Creation of responsive User Interface with SSR and hydration.
+•	Data analytics and visualization of results.
+•	API integrations with the python backend.
+•	Session-based authentication enforced by middleware.
+•	Data governance: server-side data with role-based access, 
+secure bcrypt salted hashing of user passwords.
+
+Deployment
+Collaborated with the lead SRE to deploy our microservice architecture application to an on-premises compute cluster:
+•	Containerization of microservices using Dockerfiles.
+•	Orchestration with Docker-compose, Kubernetes, Helm charts.
+•	CI/CD with Github Actions to run QA, build, and publish images.
+
+Management
+Spearheaded the project planning and management efforts: 
+•	Defined technical deliverables and established timelines.
+•	Drafted project bids to tender.
+•	Created progress reports and delivered PowerPoint presentations to effectively inform non-technical stakeholders.
+•	Assigned work to and supervised frontend developer employee.
+
+McGill University - Teaching Assistant (Engineering Economy) 
+2020 - 2021
+•	Mastered the core concepts of the engineering economy course:  Accounting principles, time value of money, fixed income investments, project evaluation, depreciation, taxation.
+•	Led office hours, tutorials, and exam reviews:  92% of students strongly agreed (64%) or agreed (28%) that I helped them. 
+
+McGill University - Graduate Researcher (Thesis)
+2019 - 2021
+•	Performed detailed scientific experimentation and data analysis to synthesize and characterize novel polymer materials.
+•	Developed proficiency in leading data-driven R&D. 
+•	Published findings in a peer-reviewed academic journal. 
+"""
